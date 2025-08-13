5689a823e57b4c3337f1bfb6a59692ca09c24546
revellan <climb.simonschmidt@web.de>
2025-08-13 08:30:05 +0200

    Write argparser

--- src/argparse.rs
+++ src/argparse.rs
@@ -1,12 +1,38 @@
-use std::io::{self, Error};
-struct GrCnf {
-    
+use std::process;
+//ERRORS
+const NO_ARGS: &str = "Not enough Arguments!";
+const TOO_MANY_ARGS: &str = "Too many Arguments!";
+const HELP_MSG: &str = "Usage: minigrep [SEARCH_STRING] [FILE]
+    If no file is given, stdin will be read instead!
+    Examples:
+        minigrep \"hello world\" hello_world.txt
+        ^- This will output every line in hello_world.txt with that contains \"hello world\"
+        
+        echo -e \"test\\nhello world hello\" | minigrep \"hello world\"
+        ^- This will output the second line \"hello world hello\", as it matches the search query";
+pub struct FileArgs<'a> {
+    pub search_query: &'a str,
+    pub file: &'a str,
 }
-pub fn parse(args: &Vec<String>) -> Result<String, io::Error> {
-    let not_enough_args: &'static str = "Not enough arguments!";
-    if args.len() == 1 {
-        return Err(Error::new(io::ErrorKind::Other, not_enough_args))
-    } else {
+pub enum Conf<'a> {
+    FromFile(FileArgs<'a>),
+    FromStdin(&'a str),
+}
+impl<'a> Conf<'a> {
+    pub fn new(args: &'a Vec<String>) -> Conf<'a> {
+        match args.len() {
+            0 | 1 => {
+                panic!("{}\n{}", NO_ARGS, HELP_MSG)
+            }
+            2 => Conf::FromStdin(&args[1]),
+            3 => Conf::FromFile(FileArgs {
+                search_query: &args[1],
+                file: &args[2],
+            }),
+            _ => {
+                eprintln!("{}\n{}", TOO_MANY_ARGS, HELP_MSG);
+                process::exit(1);
+            }
+        }
     }
-    Ok(String::from(" j"))
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,5 @@ use minigrep::argparse;
 use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let res: GrCnf = match argparse::parse(&args) {
-        Err(e) => panic!("{}", e),
-        Ok(res) => res,
-    }
+    let _res: argparse::Conf = argparse::Conf::new(&args);
 }
