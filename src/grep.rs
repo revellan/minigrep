804282240af57fccb7a5648598d647346937db4a
revellan <climb.simonschmidt@web.de>
2025-08-13 15:15:38 +0200

    Finish program, put main function into library

--- src/grep.rs
+++ src/grep.rs
@@ -1,6 +1,7 @@
-use std::{fs, io};
+use super::argparse;
+use std::{env, fs, io};
 
-pub fn stdin(search_query: &str) {
+fn stdin(search_query: &str) {
     let mut line: String = String::new();
     loop {
         line.clear();
@@ -16,7 +17,7 @@ pub fn stdin(search_query: &str) {
         }
     }
 }
-pub fn file(search_query: &str, file: &str) -> Result<(), io::Error> {
+fn file(search_query: &str, file: &str) -> Result<(), io::Error> {
     let file_contents = fs::read_to_string(file)?;
     for line in file_contents.lines() {
         if line.contains(search_query) {
@@ -24,4 +25,30 @@ pub fn file(search_query: &str, file: &str) -> Result<(), io::Error> {
         }
     }
     Ok(())
+}
+pub fn main() {
+    let args: Vec<String> = env::args().collect();
+    match argparse::Conf::new(&args) {
+        argparse::Conf::FromFile(file_args) => {
+            let error: Option<io::Error> = match file(file_args.search_query, file_args.file) {
+                Ok(_) => None,
+                Err(e) => Some(e),
+            };
+            if let Some(e) = error {
+                match e {
+                    ref e if e.kind() == io::ErrorKind::NotFound => {
+                        eprintln!("Could not find File \"{}\"", file_args.file);
+                        std::process::exit(e.raw_os_error().unwrap());
+                    }
+                    e => {
+                        eprintln!("{:?}", e.kind());
+                        std::process::exit(e.raw_os_error().unwrap());
+                    }
+                }
+            }
+        }
+        argparse::Conf::FromStdin(search_query) => {
+            stdin(search_query);
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,3 @@
-use minigrep::{argparse, grep};
-use std::{env, io};
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    match argparse::Conf::new(&args) {
-        argparse::Conf::FromFile(file_args) => {
-            let error: Option<io::Error> = match grep::file(file_args.search_query, file_args.file)
-            {
-                Ok(_) => None,
-                Err(e) => Some(e),
-            };
-            if let Some(e) = error {
-                match e {
-                    ref e if e.kind() == io::ErrorKind::NotFound => {
-                        eprintln!("Could not find File \"{}\"", file_args.file);
-                        std::process::exit(e.raw_os_error().unwrap());
-                    }
-                    e => {
-                        eprintln!("{:?}", e.kind());
-                        std::process::exit(e.raw_os_error().unwrap());
-                    }
-                }
-            }
-        }
-        argparse::Conf::FromStdin(search_query) => {
-            grep::stdin(search_query);
-        }
-    }
+    minigrep::grep::main();
 }
