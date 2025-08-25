52433e32d7ee079b9917ef85607de0b757d134e9
revellan <climb.simonschmidt@web.de>
2025-08-25 18:03:18 +0200

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod argparse;
 pub mod grep;
+mod tests;
---
+++ src/tests.rs
@@ -0,0 +1,31 @@
+#[cfg(test)]
+use super::*;
+#[test]
+fn argparse_file() {
+    use argparse::Conf;
+    use argparse::FileArgs;
+    let args: Vec<String> = ["minigrep", "test", "file.txt"]
+        .iter()
+        .map(|s| s.to_string())
+        .collect();
+    let conf: Conf = Conf::new(&args);
+    let file_args: FileArgs;
+    match conf {
+        Conf::FromStdin(_) => panic!("Argparse failed to parse Arguments correctly!!!"),
+        Conf::FromFile(c) => file_args = c,
+    }
+    assert_eq!(file_args.file, "file.txt");
+    assert_eq!(file_args.search_query, "test");
+}
+#[test]
+fn argparse_stdin() {
+    use argparse::Conf;
+    let args: Vec<String> = ["minigrep", "test"].iter().map(|s| s.to_string()).collect();
+    let conf = Conf::new(&args);
+    let search_query: String;
+    match conf {
+        Conf::FromFile(_) => panic!("Argparse failed to parse Arguments correctly!!!"),
+        Conf::FromStdin(s) => search_query = s.to_string(),
+    }
+    assert_eq!(search_query, "test");
+}
