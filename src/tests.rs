889b9906c53a916513707a831d79b592eccca1d1
revellan <climb.simonschmidt@web.de>
2025-08-26 11:08:36 +0200

    Improve tests

--- src/tests.rs
+++ src/tests.rs
@@ -4,7 +4,7 @@ use super::*;
 fn argparse_file() {
     use argparse::Conf;
     use argparse::FileArgs;
-    let args: Vec<String> = ["minigrep", "test", "file.txt"]
+    let args: Vec<String> = ["minigrep", "searchstring", "file.txt"]
         .iter()
         .map(|s| s.to_string())
         .collect();
@@ -15,17 +15,17 @@ fn argparse_file() {
         Conf::FromFile(c) => file_args = c,
     }
     assert_eq!(file_args.file, "file.txt");
-    assert_eq!(file_args.search_query, "test");
+    assert_eq!(file_args.search_query, "searchstring");
 }
 #[test]
 fn argparse_stdin() {
     use argparse::Conf;
-    let args: Vec<String> = ["minigrep", "test"].iter().map(|s| s.to_string()).collect();
+    let args: Vec<String> = ["minigrep", "searchstring"].iter().map(|s| s.to_string()).collect();
     let conf = Conf::new(&args);
     let search_query: String;
     match conf {
         Conf::FromFile(_) => panic!("Argparse failed to parse Arguments correctly!!!"),
         Conf::FromStdin(s) => search_query = s.to_string(),
     }
-    assert_eq!(search_query, "test");
+    assert_eq!(search_query, "searchstring");
 }
